e12aba457d0d9aa57858cd9548baeffaa2d9a6ac
Nabeel Nasir <nabeeln7@gmail.com>
2019-10-23 22:04:34 -0400

    all timing done at the first gw

--- discover-mock-sensor-ble.js
+++ discover-mock-sensor-ble.js
@@ -3,10 +3,17 @@ var mqtt  = require('mqtt');
 var request = require('request');
 
 var MQTT_TOPIC_NAME = 'mock-sensor-data';
+var ACK_MQTT_TOPIC_NAME = 'ack-topic';
 var mqtt_client = mqtt.connect('mqtt://localhost');
+
+cloudIP = "198.199.83.16";
+cloudPort = 5000;
 
 cloudletIP = "172.27.44.129";
 cloudletPort = 5000;
+
+// packetTs => sendTime
+pendingACKPackets = {}
 
 function getCurrentDateTime() {
   return new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
@@ -25,6 +32,14 @@ function handleNobleStateChange(state) {
   }
 }
 
+function sendToServer(ipAdrr, port, label) {
+  var cloudStartTime = Date.now();
+  request.get(`http://${ipAdrr}:${port}/record?packetTs=1234`, function (error, response, body) {
+    var cloudEndTime = Date.now();
+    console.log(`${label} latency = ${cloudEndTime - cloudStartTime}`);
+  });
+}
+
 function handleDiscoveredPeripheral(peripheral) {
   if (!peripheral.advertisement.manufacturerData) {
     // console.log("[BLE Radio] Peripheral discovered: " + peripheral.address);
@@ -35,17 +50,14 @@ function handleDiscoveredPeripheral(peripheral) {
       if (data.startsWith('mock-sensor')) {
         // console.log(`[BLE Radio] Received advertisement data = ${data}`);
         
-        //record the time of receiving the sensor data
-        receiveTime = Date.now();
-        packetTs = parseInt(data.split("#")[1]);
-        latency = receiveTime - packetTs;
-        console.log(`${packetTs}\t${latency}`);
-
-        //send stuff to the http server
-        request.get(`http://${cloudletIP}:${cloudletPort}/record?packetTs=${packetTs}`);
+        sendToServer(cloudIP, cloudPort, "cloud")
+        sendToServer(cloudletIP, cloudletPort, "cloudlet")
+        
 
         //publish packet in mqtt topic
+        receiveTime = Date.now();
         mqtt_client.publish(MQTT_TOPIC_NAME, JSON.stringify(data));
+        pendingACKPackets[packetTs] = receiveTime;
       }
     }
   }
@@ -57,4 +69,22 @@ mqtt_client.on('connect', function () {
     //we do noble's listener initialization here as there's a dependency on ranging key and iv
     noble.on('stateChange', handleNobleStateChange);
     noble.on('discover', handleDiscoveredPeripheral);
+
+    client.subscribe(ACK_MQTT_TOPIC_NAME);
+});
+
+client.on('message', (topic, message) => {
+  if(topic === ACK_MQTT_TOPIC_NAME) {
+    data = JSON.parse(message.toString());
+
+    //record the time of receiving the ack
+    ackReceiveTime = Date.now();
+    packetTs = parseInt(data.split("#")[1]);
+    
+    receiveTime = pendingACKPackets[packetTs]
+    latency = ackReceiveTime - receiveTime;
+    console.log(`mqtt ack received for ${packetTs} latency = ${latency}`);
+
+    delete pendingACKPackets[packetTs]
+  }
 });
--- packet-time-recorder.js
+++ packet-time-recorder.js
@@ -1,9 +1,11 @@
 // given a sensor-id and a source-ip-address, listens to mqtt on that ip address and records the time that the packets were received
 const mqtt = require('mqtt')
 
+var ACK_MQTT_TOPIC_NAME = 'ack-topic';
+
 sourceIP = process.argv[2];
 
-const client = mqtt.connect(`mqtt://${sourceIP}`)
+const client = mqtt.connect(`mqtt://${sourceIP}`);
 
 client.on('connect', () => {
   client.subscribe('mock-sensor-data')
@@ -11,12 +13,15 @@ client.on('connect', () => {
 
 client.on('message', (topic, message) => {
   if(topic === 'mock-sensor-data') {
-    data = JSON.parse(message.toString());
+    // data = JSON.parse(message.toString());
 
-    //record the time of receiving the sensor data
-    receiveTime = Date.now();
-    packetTs = parseInt(data.split("#")[1]);
-    latency = receiveTime - packetTs;
-    console.log(`${packetTs}\t${latency}`);
+    // //record the time of receiving the sensor data
+    // receiveTime = Date.now();
+    // packetTs = parseInt(data.split("#")[1]);
+    // latency = receiveTime - packetTs;
+    // console.log(`${packetTs}\t${latency}`);
+
+    client.publish(ACK_MQTT_TOPIC_NAME, message.toString());
+    console.log("received message. ack sent");
   }
 });
